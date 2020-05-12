62b08868c9f1196cbff191112da0525c9ccebc14
naresh sharma <sobarguy.naresh@yahoo.co.in>
2020-05-12 18:32:01 +0530

    SecondtCommit

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -1,19 +1,27 @@
 package testcases;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
 
-	@Test
-	public void verifyTitle() {
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void doLaunchBrowser() {
 
 		System.setProperty("webdriver.chrome.driver",
-				System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
+				System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\chromedriver.exe");
 
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 
 		driver.manage().window().maximize();
 
@@ -21,14 +29,46 @@ public class TestCase1 {
 
 		driver.get("http://www.google.com");
 
-		String title = driver.getTitle();
-		
-		System.out.println("title is :"+title);
-		
-		Assert.assertEquals(title,"Google","Title not match");
-		
-		driver.quit();
+	}
 
+	@Test
+	public void verifyTitle() {
+
+		String title = driver.getTitle();
+
+		System.out.println("title is :" + title);
+
+		Assert.assertEquals(title, "Google", "Title not match");
+
+	}
+
+	@Test
+	public void verifUrl() {
+
+		String url = driver.getCurrentUrl();
+
+		System.out.println("Url is :" + url);
+	}
+
+	@Test
+	public void getAllLinks() {
+
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		System.out.println("total links are:" + links.size());
+		
+		for(WebElement ele: links) {
+			
+			String str=ele.getAttribute("href");
+			
+			System.out.println("link is :"+str);
+		}
+	}
+
+	@AfterSuite
+	public void closeBrowser() {
+
+		driver.quit();
 	}
 
 }
